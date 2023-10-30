7c2e91b87ca85859c208264d1ee415a27460e8df
Mubeena Udyavar Kazi <mubeena.udyavarkazi@colorado.edu>
2023-10-29 20:37:06 -0600

    Updated commit version to use fixed aesdsocket

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '4316a839d08859240e633beca03a9c19e6ce55f6'
+AESD_ASSIGNMENTS_VERSION = 'f9e1996b702f442b095f3af54d372410f32ba2ab'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
