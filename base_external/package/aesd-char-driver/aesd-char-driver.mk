d37b206abf525de59e8a74cd60c1120ba92b7498
Mubeena Udyavar Kazi <mubeena.udyavarkazi@colorado.edu>
2023-10-29 19:26:33 -0600

    Added aesd-char-driver package

---
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -0,0 +1,25 @@
+
+##############################################################
+#
+# AESD-CHAR-DRIVER
+#
+##############################################################
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESD_CHAR_DRIVER_VERSION = '5525ef0c5433cecfe97fd065cfd97472f841a13b'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESD_CHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-mubeena12.git'
+AESD_CHAR_DRIVER_SITE_METHOD = git
+AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+
+AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+
+define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
