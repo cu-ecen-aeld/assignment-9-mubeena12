c6fa2ae05010040cbf5849be65622e9d6c36822d
Mubeena Udyavar Kazi <mubeena.udyavarkazi@colorado.edu>
2023-11-07 06:26:25 -0700

    Updated to use the latest version of  assignment 9 of aesdsocket and aesd-char-driver

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'b85c53c1d359b0091822f61e117f0080c71d4436'
+AESD_ASSIGNMENTS_VERSION = '59888d35d8d785e2ba5146622ccc27cf8418300d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
